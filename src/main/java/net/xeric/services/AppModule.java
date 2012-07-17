65fc233aac0330f65b6327cfd453e9174cd84420
Taha Hafeez Siddiqi <tawushafeez@gmail.com>
2012-07-17 10:58:11 +0530

    Added DBStarter configuration

--- src/main/java/net/xeric/services/AppModule.java
+++ src/main/java/net/xeric/services/AppModule.java
@@ -112,9 +112,9 @@ public class AppModule {
 
                 unitInfo
                         .addProperty("javax.persistence.jdbc.driver", "org.h2.Driver")
-                        .addProperty("javax.persistence.jdbc.url", "jdbc:h2:mem:test")
+                        .addProperty("javax.persistence.jdbc.url", "jdbc:h2:tcp://localhost/mem::test;create=true;DB_CLOSE_ON_EXIT=FALSE")
                         .addProperty("javax.persistence.jdbc.username", "sa")
-                        .addProperty("javax.persistence.jdbc.username", "")
+                        .addProperty("javax.persistence.jdbc.password", "password")
                         .addProperty("eclipselink.ddl-generation", "create-tables")
                         .addProperty("eclipselink.logging.level", "fine");
             }
